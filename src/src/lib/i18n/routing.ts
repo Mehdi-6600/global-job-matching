42351d4a37b8592352a9e0579a6a3598ce410793
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-19 02:57:10 +0330

    Delete src/src directory

--- src/src/lib/i18n/routing.ts
+++
@@ -1,9 +0,0 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation";
-
-export const locales = ["en", "fa", "ar", "es", "fr", "hi"] as const;
-export const defaultLocale = "en";
-
-export const { Link, usePathname, useRouter } = createSharedPathnamesNavigation({
-  locales,
-  defaultLocale,
-});
