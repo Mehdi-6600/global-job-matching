94ec95b5c9e24d4e336570a9dfb59078c6b30ca8
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-18 23:20:37 +0330

    Remove Next Intl plugin from next.config.js
    Removed Next Intl plugin integration from configuration.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const createNextIntlPlugin = require("next-intl/plugin");
-
-const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
