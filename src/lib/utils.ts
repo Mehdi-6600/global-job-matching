b007c4a246397962dc38054155cb473804870e0a
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-19 04:46:40 +0330

    Correct tailwind-merge import in utils.ts
    Fix import statement for tailwind-merge.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge } from "tailwind-merge";  // ← درستش کن
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
