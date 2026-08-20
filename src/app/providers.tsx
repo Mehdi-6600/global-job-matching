932ff0c05589292898d0b10e18bfa74dc98c3005
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-21 00:40:43 +0330

    Add Providers component for theme and notifications

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { NotificationProvider } from "@/components/notification-provider";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange={false}
+    >
+      <NotificationProvider>{children}</NotificationProvider>
+    </ThemeProvider>
+  );
+}
