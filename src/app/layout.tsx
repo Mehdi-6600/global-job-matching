4e12d9ea837d5218a93b14a77a268ac331a55572
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-22 00:51:51 +0330

    Add Navbar component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";   // ← f کوچیک
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased flex flex-col min-h-screen`}>
+        <Navbar />
         <div className="flex-grow">{children}</div>
         <Footer />
       </body>
