868d474c303a7dc83d71053dab652d1b44aab5ad
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-22 06:19:17 +0330

    Change Footer component to named export

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Footer() {
+export function Footer() {
   return (
     <footer className="glass-strip mt-auto">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
