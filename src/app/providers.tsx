08ff41358c212d6c73f8957cc93b3f39949b829e
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-08-27 06:03:29 +0330

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { NotificationProvider } from "@/components/notification-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange={false}
-    >
-      <NotificationProvider>{children}</NotificationProvider>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange={false}
+      >
+        <NotificationProvider>{children}</NotificationProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
