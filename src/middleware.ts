0ff57b6b8b9c91c0f4d3d3490dd469eb644fe375
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-09-05 14:02:32 +0330

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
-    /*
-     * همه چیز به‌جز static و فایل‌های با پسوند
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)",
   ],
 };
