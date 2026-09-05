395d9a84b572394202e25edb2a0818ee9407ecac
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-09-05 13:49:51 +0330

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import "next-auth";
+import "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
@@ -9,8 +10,19 @@ declare module "next-auth" {
       email?: string | null;
       image?: string | null;
     };
+    error?: string;
   }
+
   interface User {
     role: string;
+    sessionVersion?: number;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: string;
+    sessionVersion?: number;
+    error?: "SessionInvalidated" | "RateLimited";
   }
 }
