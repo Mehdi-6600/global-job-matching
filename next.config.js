b4dba16ab898f86b2a5dbb5ed7c574dfde515f7e
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-09-06 14:24:17 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -60,6 +60,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600, stale-while-revalidate=86400",
+          },
+        ],
+      },
     ];
   },
 };
