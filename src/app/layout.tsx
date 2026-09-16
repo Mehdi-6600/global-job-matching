4e3c80e5e230bd2477d6200d9c042506b33029b8
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-09-16 06:03:07 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 import "./globals.css";
-import "./ui-styleguide.css";
 import Navbar from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { Providers } from "./providers";
