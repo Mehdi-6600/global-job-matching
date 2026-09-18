b9329503e845364fbce63c2a278fff798e9c132c
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-09-18 16:26:36 +0330

    Create index.ts

---
+++ src/lib/jobs/ingestion/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./registry";
+export * from "./quality";
+export * from "./dedup";
+export * from "./occupation";
+export { runIngestion } from "./pipeline";
