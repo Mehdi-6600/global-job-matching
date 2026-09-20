a65e28220d42dafd36c57c0086065acbfeb22117
Mehdi-6600 <veysi.mehdi@gmail.com>
2026-09-20 15:39:13 +0330

    Update session-provider.tsx

--- src/components/session-provider.tsx
+++ src/components/session-provider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-export default function AuthProvider({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function SessionProvider({ children }: { children: ReactNode }) {
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
 }
